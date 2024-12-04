40746e9994fa59ba5942772e195932bcb23a1b34
Marc Koehler <96128291+marckoehler@users.noreply.github.com>
2024-12-04 15:17:58 +0100

    Updated ElementContainerVisitor to also work with new logic for handling inline types.

--- src/Metadata/Converter/Types/Visitor/ElementContainerVisitor.php
+++ src/Metadata/Converter/Types/Visitor/ElementContainerVisitor.php
@@ -9,6 +9,7 @@ use GoetasWebservices\XML\XSDReader\Schema\Element\ElementItem;
 use GoetasWebservices\XML\XSDReader\Schema\Element\ElementSingle;
 use GoetasWebservices\XML\XSDReader\Schema\Element\Group;
 use GoetasWebservices\XML\XSDReader\Schema\Element\Sequence;
+use GoetasWebservices\XML\XSDReader\Schema\Type\ComplexType;
 use Soap\Engine\Metadata\Collection\PropertyCollection;
 use Soap\Engine\Metadata\Model\Property;
 use Soap\Engine\Metadata\Model\XsdType as EngineType;
@@ -24,19 +25,24 @@ final class ElementContainerVisitor
         return new PropertyCollection(
             ...flat_map(
                 $container->getElements(),
-                fn (ElementItem $element): PropertyCollection => $this->parseElementItem($element, $context)
+                fn (ElementItem $element): PropertyCollection => $this->parseElementItem($element, $context, $container)
             )
         );
     }
 
-    private function parseElementItem(ElementItem $element, TypesConverterContext $context): PropertyCollection
+    private function parseElementItem(ElementItem $element, TypesConverterContext $context, ComplexType $parent): PropertyCollection
     {
         if ($element instanceof Group || $element instanceof Choice || $element instanceof Sequence) {
-            return $this->__invoke($element, $context);
+            return new PropertyCollection(
+                ...flat_map(
+                    $element->getElements(),
+                    fn (ElementItem $child): PropertyCollection => $this->parseElementItem($child, $context, $parent)
+                )
+            );
         }
 
-        $type = $element instanceof ElementSingle ? $element->getType() : null;
-        $typeName = $type?->getName() ?: $element->getName();
+        $typeName = $this->parseTypeName($element, $parent);
+
         $configure = pipe(
             static fn (EngineType $engineType): EngineType => (new Configurator\ElementConfigurator())($engineType, $element, $context),
         );
@@ -47,5 +53,30 @@ final class ElementContainerVisitor
                 $configure(EngineType::guess($typeName))
             )
         );
+    }
+
+    private function parseTypeName(ElementItem $element, ComplexType $parent): string
+    {
+        // If ElementItem is not ElementSingle (e.g. Any), we guess the type from the element name later on.
+        if (!$element instanceof ElementSingle) {
+            return $element->getName();
+        }
+
+        // If ElementSingle is local, the type will be the element name prefixed with the parent name
+        // to avoid name conflicts with other (inline) elements of same name.
+        if ($element->isLocal()) {
+            return $parent->getName() . (ucfirst($element->getName()));
+        }
+
+        // If ElementSingle is global, the type will be its type name.
+        $typeName = $element->getType()->getName();
+
+        // However, if this type name is null, we have a global element with an anonymous complex type.
+        // The element name will be the type name instead.
+        if ($typeName === null) {
+            $typeName = $element->getName();
+        }
+
+        return $typeName;
     }
 }
